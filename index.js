a3d494e6535a70f5120b4c428eb705ec66647461
Taufiq-survival <taufiqabdulrahman9@gmail.com>
2024-11-06 13:36:45 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,133 @@
+
+let department = 'programming';
+console.log(department)
+
+   let grade = 'A'
+   console.log(grade)
+
+   let name = 'Taufiq,hadrat,usman'
+   console.log(name)
+
+   let age = '1';
+   age = 2;
+   console.log(age)
+
+   let firstname = 'kwaku'
+   let firstName = 'kwako'
+   console.log(firstname);
+   console.log(firstName);
+
+   let sum= 200 + 700;
+   console.log(sum)
+
+   let div= 1500 / 3;
+   console.log(div)
+
+   let multiplication= 200 * 3;
+   console.log(multiplication)
+   
+   let Y= 200;
+   Y +=50
+   console.log(Y);
+
+
+
+
+//define a variable age and assign the value 17 to it.
+//let agee = 17;
+
+//if (agee < 18)
+   //console.log("you are not eligible");
+
+for (let i = 0; i < 5; i++) {
+   console.log(i);
+}
+
+function greet () {
+console.log("hello, world");
+}
+
+greet()
+
+
+
+
+//function greetUser(firstame, lastnanme) {
+console.log("hello" + name)
+
+//greetUser ("akosua")
+
+
+//function addition(a,b) {
+//console.log(10+5)
+
+
+//addition(10,5)
+
+function addAndlog(a, b) { 
+let result = a+ b;
+return result;
+console.log('addition'(10+5))
+}
+
+
+
+//const greeting = () => {
+//let message = 'hello';
+//console.log(message)
+//greeting();
+
+
+
+//function calculateArea(width , height) {
+//let area = width * height;
+//return result
+
+
+//console.log('Area'(5 * 10))
+
+
+//let students = ['chrisry', 'gloria', 'lois', 'prince', 'sly']
+//console.log(students[students.length - 1]);
+
+//let students = ['christy','gloria','lois','prince','sly']
+//console.log(students[2]);
+
+let courses = ['maths', 'english', 'science', 'social'];
+console.log(courses);
+console.log(courses.pop());
+console.log(courses);
+
+courses.push('biology');
+console.log(courses)
+
+courses.unshift('chemistry' , 'physics')
+console.log(courses)
+
+console.log(courses.indexOf('english'));
+(courses.shift('maths'))
+console.log(courses)
+
+//for (let i = 0; i < courses.length; i++){
+  // console.log(i);
+//console.log(courses[i])
+
+for (let i = 0; i < courses.length; i+=2) {
+console.log(i);
+console.log(courses[i])
+}
+
+let fruits = ['mango', 'apple', 'banana', 'cherry'];
+fruits.forEach((fruits) => {
+console.log(fruits);
+})
+
+//object
+let students = {
+firstName: 'baba',
+lastname: 'kumasi'
+
+
+
+}
+ 
